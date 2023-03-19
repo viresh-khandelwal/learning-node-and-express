a675a959bac16e1661458f8c91fd9b94135f73b2
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2023-03-19 15:04:42 +0530

    updated DB URL logic

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const db_user_name = 'viresh';
 const db_pwd = 'Viresh%4019'; //actual password is Viresh@19 but @ needs to be url encoded to %40
-const DB_URL = 'mongodb+srv://viresh:'+db_pwd+'@mongo-db-learning.82bygnn.mongodb.net/?retryWrites=true&w=majority';
+const DB_URL = `mongodb+srv://${db_user_name}:${db_pwd}@mongo-db-learning.82bygnn.mongodb.net/?retryWrites=true&w=majority`;
 
 // The major difference between require and import , is that require will automatically scan node_modules to find modules, but import , which comes from ES6, won't. Most people use babel to compile import and export , which makes import act the same as require .
 
